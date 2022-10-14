9c32193cbc1b394c9d58eddc217d2b1855db7fd8
Kamil GÖRÜCÜ <98644346+KamilGorucu@users.noreply.github.com>
2022-10-14 15:51:22 +0300

    mekanbul-2

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const anaSayfa = function (req, res, next) {
-    res.render('anasayfa', { title: 'AnaSayfa' })
+const anaSayfa = function (req, res) {
+    res.render('anasayfa', { "title": "AnaSayfa" })
 }
-const mekanBilgisi = function (req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' })
+const mekanBilgisi = function (req, res) {
+    res.render('mekanbilgisi', { "title": "Mekan Bilgisi" })
 }
-const yorumEkle = function (req, res, next) {
-    res.render('yorumekle', { title: 'Yorum Sayfasi' })
+const yorumEkle = function (req, res) {
+    res.render('yorumekle', { "title": "Yorum Sayfasi" })
 }
 module.exports = {
 
